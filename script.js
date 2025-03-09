41408d9d269da1b3c293fa01a71cb769a3e81710
girtabb <131411046+girtabb@users.noreply.github.com>
2025-03-09 23:05:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+window.onload = () => {
+    const summaryItems = document.querySelectorAll('.summary-item');
+    let delay = 0;
 
+    summaryItems.forEach(item => {
+        setTimeout(() => {
+            item.style.animationDelay = '0s';
+            item.style.opacity = 1;
+        }, delay);
+        delay += 300; // Delay for each item to animate one after another
+    });
+};
+
+function startAnalysis() {
+    alert("Analysiere die Episode: Angelo versucht, seinen Bruder mit Süßigkeiten ruhigzustellen – doch es geht alles schief!");
+}
